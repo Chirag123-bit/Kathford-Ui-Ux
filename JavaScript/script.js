866d51b34505177347882e610faafcecff454478
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-03-13 13:30:17 +0545

    Javascript Started

---
+++ JavaScript/script.js
@@ -0,0 +1,53 @@
+// console.log("Hello World");
+// alert("This is an alert");
+
+// var userName = prompt("Enter Your Name: ");
+// console.log(userName);
+
+// Let, const, var
+
+// Data Types (String, Boolean, Numbers, Array, Objects)
+
+// String
+var str1 = "Hello";
+var str2 = "2";
+console.log(str1 + str2);
+
+// Numbers
+var num1 = 10;
+var num2 = 10.5;
+var var4 = "a";
+console.log(num1 * var4);
+
+// Booleans
+var bool1 = true;
+var bool2 = false;
+
+// Arrays
+var arr1 = [1, 2, "Hello", 10.22, [1, 2, 3]];
+
+// Objects
+// const obj1 = {
+//   name: "Chirag",
+//   age: 30,
+//   address: "Kathmandu",
+// };
+// obj1.name = "asdsadasds";
+// console.log(obj1);
+// console.log(obj1.name);
+
+// Array Methods (Push, pop, slice, splice, toString, join,
+//               shift, unshift, concat, splice, slice)
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arr2);
+// arr2.push(10);
+// arr2.pop();
+// arr2.shift();
+// arr2.unshift(10);
+// console.log(arr2.slice(4));
+// arr2.splice(4, 0, 10,50,60);
+// let arr3 = arr2.toString();
+// let arr3 = arr2.join(" ");
+// console.log(arr3);
+console.log(arr1.concat(arr2));
