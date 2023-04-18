51a31cb66ccb3fba4334287a9e7165c582b061d8
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-04-18 12:01:47 +0545

    Layouts

---
+++ bookingapp/src/pages/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./todo.css"
+
+const Todo = () => {
+    return (
+        <div id='todoPage'>
+            <div className="todoPageContainer">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Todo
